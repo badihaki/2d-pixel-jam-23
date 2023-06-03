221b1ea1ab58545b10a8d8d8d2bf222ab73a157e
badihaki <jojackblack@gmail.com>
2023-06-03 02:55:13 -0400

    refactored Player Controls. Created Health and CheckGround scripts for global game objects

--- Assets/Player/Scripts/PlayerControls.cs
+++ Assets/Player/Scripts/PlayerControls.cs
@@ -10,17 +10,26 @@ public class PlayerControls : MonoBehaviour
     public void OnMove(InputValue value) => ProcessMove(value.Get<Vector2>());
     [field:SerializeField] public bool Jump { get; private set; }
     public void OnJump(InputValue value) => ProcessJump(value.isPressed);
+    public void UseJump() => Jump = false;
     [field: SerializeField] public bool Attack { get; private set; }
     public void OnAttack(InputValue value) => ProcessAttack(value.isPressed);
+    public void UseAttack() => Attack = false;
     [field: SerializeField] public bool Weapon { get; private set; }
     public void OnWeapon(InputValue value) => ProcessWeapon(value.isPressed);
+    public void UseWeapon() => Weapon = false;
     [field: SerializeField] public bool Grab { get; private set; }
     public void OnGrab(InputValue value) => ProcessGrab(value.isPressed);
+    public void UseGrab() => Grab = false;
     
     // Process Inputs Below
     private void ProcessMove(Vector2 input)
     {
-        Move = input;
+        Vector2 modifiedInput = input;
+        if (modifiedInput.x > 0) modifiedInput.x = 1;
+        else if (modifiedInput.x < 0) modifiedInput.x = -1;
+        if (modifiedInput.y > 0) modifiedInput.y = 1;
+        else if (modifiedInput.y < 0) modifiedInput.y = -1;
+        Move = modifiedInput;
     }
     private void ProcessJump(bool input)
     {
